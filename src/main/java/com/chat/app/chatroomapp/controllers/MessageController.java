132d67e8e5d21d861e5a48df21ca4a91b0c0a082
swap-2149 <patilswapnil2149@gmail.com>
2024-04-19 20:56:48 +0530

    initial commit

---
+++ src/main/java/com/chat/app/chatroomapp/controllers/MessageController.java
@@ -0,0 +1,24 @@
+package com.chat.app.chatroomapp.controllers;
+
+import com.chat.app.chatroomapp.models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) {
+
+//        try {
+//            //processing
+////            Thread.sleep(2000);
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return message;
+    }
+}
